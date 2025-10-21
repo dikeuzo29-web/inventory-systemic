703942bd517348382b4019fa7a4c92e7a528d905
Dike Uzo <dikeuzo29@gmail.com>
2025-10-21 15:25:00 +0100

    Implement advanced multi-tenant admin for users

--- tenants/admin.py
+++ tenants/admin.py
@@ -1,7 +1,3 @@
-# tenants/admin.py
-
 from django.contrib import admin
-from .models import Client
 
-# This lets you create/edit tenants in the admin
-admin.site.register(Client)
+# Register your models here.
