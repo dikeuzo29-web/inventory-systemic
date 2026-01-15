c200ef3e8120985baacf9ec41f78a578c48769ac
dikeuzo29-web <dikeuzo29@gmail.com>
2026-01-15 20:12:49 +0100

    Update tenant_urls.py

--- inventory_systems/tenant_urls.py
+++ inventory_systems/tenant_urls.py
@@ -1,34 +1,14 @@
-# inventory_systems/urls.py
-from django.contrib import admin
+# inventory_systems/tenant_urls.py
 from django.urls import path, include
-from django.conf import settings
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.views.generic import RedirectView
 
-def homepage(request):
+def tenant_home(request):
     return render(request, "stock/homepage.html")
 
 urlpatterns = [
-    # Public homepage
-    path("", homepage, name="home"),
-
-    # ✅ ENABLE SUBFOLDER TENANTS
-    path(
-        f"{settings.TENANT_SUBFOLDER_PREFIX}/",
-        include(settings.TENANT_URLCONF),
-    ),
-
-    # Public APIs
+    path("", tenant_home, name="tenant_home"),
     path("api/accounts/", include("accounts.urls")),
     path("api/stock/", include("stock.urls")),
-
-    # PWA + offline
     path("offline/", TemplateView.as_view(template_name="offline.html")),
-
-    # Admin
-    path("admin/", admin.site.urls),
 ]
-
-#     path("", include("pwa.urls")),                                  # PWA routes
-# ]
