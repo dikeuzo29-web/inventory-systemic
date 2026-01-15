57d81286f27c90b9bac93dd231a37ec60f7358ff
dikeuzo29-web <dikeuzo29@gmail.com>
2026-01-15 20:16:26 +0100

    Update urls.py

--- inventory_systems/urls.py
+++ inventory_systems/urls.py
@@ -53,6 +53,11 @@ def chart_data(request):
 
 urlpatterns = [
     path("", homepage, name="home"),
+
+    path(
+        f"{settings.TENANT_SUBFOLDER_PREFIX}/",
+        include(settings.TENANT_URLCONF),
+    ),
     
     # PWA routes - ADD THES
    path('sw.js', sw), 
