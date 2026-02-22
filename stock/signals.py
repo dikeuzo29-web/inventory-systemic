ca777b617f5356751df3616f489da4991e775cc5
dikeuzo29-web <dikeuzo29@gmail.com>
2026-02-22 12:20:01 +0100

    Update signals.py

--- stock/signals.py
+++ stock/signals.py
@@ -1,5 +1,6 @@
 import logging
 from django.db.models.signals import post_save, pre_delete
+from django.dispatch import receiver
 from django.db import transaction
 from .models import Transaction, Product, Sale, SaleItem
 
