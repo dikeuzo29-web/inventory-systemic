8b3580a1a0a5a8987543cec8f0a682f8f259f746
dikeuzo29-web <dikeuzo29@gmail.com>
2026-02-22 11:47:06 +0100

    Update signals.py

--- stock/signals.py
+++ stock/signals.py
@@ -1,7 +1,8 @@
 import logging
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre-delete
 from django.dispatch import receiver
-from .models import Transaction, Product
+from django.db import transaction
+from .models import Transaction, Product, Sale, Saletem
 
 audit_logger = logging.getLogger('audit')
 
@@ -31,3 +32,20 @@ def update_product_quantity(sender, instance, created, **kwargs):
             f"Transaction {instance.id}: {instance.transaction_type} of {instance.quantity} "
             f"units on product {product.id} by user {instance.created_by}"
         )
+
+@receiver(pre_delete, sender=Sale)
+def restore_stock_when_sale_deleted(sender, instance, **kwargs):
+    """
+    When a Sale is deleted (including from Admin),
+    restore stock for all its SaleItems.
+    """
+
+    with transaction.atomic():
+        for item in instance.items.all():
+            # Return quantity back to stock
+            item.product.adjust_stock(item.quantity)
+
+
+@receiver(pre_delete, sender=SaleItem)
+def restore_stock_when_item_deleted(sender, instance, **kwargs):
+    instance.product.adjust_stock(instance.quantity)
