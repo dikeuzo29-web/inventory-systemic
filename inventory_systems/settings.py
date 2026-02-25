dcd731d47f42b721d1e1b6b10fd022d3113d1442
dikeuzo29-web <dikeuzo29@gmail.com>
2026-02-25 09:11:58 +0100

    Change conn_max_age to 0 in database settings
    Set connection max age to 0 for database configuration.

--- inventory_systems/settings.py
+++ inventory_systems/settings.py
@@ -152,7 +152,7 @@ import dj_database_url
 DATABASES = {
     "default": dj_database_url.config(
         default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
+        conn_max_age=0,
         ssl_require=True,
     )
 }
